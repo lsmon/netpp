4dadcecfb81a88b6576f5f6aeb98ba47699ae9fc
Salomon Lee <lsh714@gmail.com>
2024-05-02 09:40:01 -0700

    Creating a class that can contain all data related to http requests for future expansion

---
+++ src/Main.cpp
@@ -0,0 +1,14 @@
+
+#include "Server.hpp"
+#include "http/HttpMethod.hpp"
+
+int main() {
+    Server serverHttp("localhost", "8080", 1024, 4);
+
+    serverHttp.setHttpHandler(HttpMethod::GET, "/", [](const HttpRequest&, HttpResponse& response) {
+        response.body = "Hello, World!";
+    });
+
+    serverHttp.run();
+    return 0;
+}
---
+++ src/http/HttpRequest.cpp
@@ -0,0 +1,66 @@
+#include "HttpRequest.hpp"
+
+void HttpRequest::setHeaders(const std::unordered_map<std::string, std::string> value)
+{
+    headers = value;
+}
+
+void HttpRequest::addHeader(const std::string &key, const std::string &value)
+{
+    headers[key] = value;
+}
+
+std::unordered_map<std::string, std::string> HttpRequest::getHeaders() const
+{
+    return headers;
+}
+
+void HttpRequest::setMethod(const std::string value)
+{
+    method = value;
+}
+
+std::string HttpRequest::getMethod() const
+{
+    return method;
+}
+
+void HttpRequest::setPath(const std::string &value)
+{
+    path = value;
+}
+
+std::string HttpRequest::getPath() const
+{
+    return path;
+}
+
+void HttpRequest::setQueryString(const std::unordered_map<std::string, std::string> value)
+{
+    queryString = value;
+}
+
+std::unordered_map<std::string, std::string> HttpRequest::getQueryString() const
+{
+    return queryString;
+}
+
+void HttpRequest::setFragment(const std::string &value)
+{
+    fragment = value;
+}
+
+std::string HttpRequest::getFragment() const
+{
+    return fragment;
+}
+
+void HttpRequest::setBody(const std::string &value)
+{
+    body = value;
+}
+
+std::string HttpRequest::getBody() const
+{
+    return body;
+}
