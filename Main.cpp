5f4c6c841c51369e55c87fddd339e94e4a950342
Salomon Lee <lsh714@gmail.com>
2024-05-30 01:06:34 -0700

    Modifying Main.cpp to become a test source

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,71 @@
 #include "http/HttpServer.hpp"
 #include "http/HttpMethod.hpp"
+#include "http/HttpClient.hpp"
+#include <string.h>
 
-int main() {
+void testHttpServer()
+{
     HttpServer serverHttp("localhost", "8080", 1024, 4);
 
-    serverHttp.setHttpHandler(HttpMethod::GET, "/", [](const HttpRequest&, HttpResponse& response) {
-        response.body = "Hello, World!";
-    });
+    serverHttp.setHttpHandler(HttpMethod::GET, "/greetings", [](const HttpRequest &, HttpResponse &response)
+                              { response.body = "Hello, World!"; });
 
     serverHttp.run();
-    return 0;
+}
+
+void testHttpClient()
+{
+    try
+    {
+        HttpClient client;
+        std::string url = "http://localhost:8080/greetings";
+        std::string response = client.get(url);
+        std::cout << "GET Response: " << response << std::endl;
+    }
+    catch (const std::exception &ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+    }
+}
+
+
+void testHttpsClient()
+{
+    try
+    {
+        HttpClient client;
+        std::string url = "https://xcapi.alcacruz.com/contest";
+        std::string response = client.get(url);
+        std::cout << "GET Response: " << response << std::endl;
+    }
+    catch (const std::exception &ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+    }
+}
+
+int main(int argc, char **args)
+{
+    if (argc != 2)
+    {
+        testHttpServer();
+        return EXIT_FAILURE;
+    }
+    if (strcmp(args[1], "server") == 0)
+    {
+        testHttpServer();
+    }
+    else if (strcmp(args[1], "client") == 0)
+    {
+        testHttpClient();
+    } 
+    else if (strcmp(args[1], "https") == 0)
+    {
+        testHttpsClient();
+    }
+    else 
+    {
+        std::cout << args[1] << std::endl;
+    }
+    return EXIT_SUCCESS;
 }
